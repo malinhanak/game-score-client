91b5750e2b2e3204c7d842af2831575f7fe32236
Malin Hanak <malinehanak@gmail.com>
2020-08-08 11:06:56 +0200

    shared helpers and exports

--- src/shared/helpers.js
+++ src/shared/helpers.js
@@ -1,29 +1,4 @@
+import { useState } from 'react';
 import moment from 'moment';
 
-export const getGames = async () => {
-  const year = await moment(new Date()).format('YYYY');
-  console.log('year', year);
-  const res = await fetch(`http://localhost:4000/api/game/${year}`, {
-    method: 'GET',
-    headers: { 'content-type': 'application/json' }
-  });
-
-  const games = await res.json();
-  console.log('games', games);
-
-  return games;
-};
-
-export const fetchData = async (token) => {
-  const year = await moment(new Date()).format('YYYY');
-  const res = await fetch(`http://localhost:4000/api/game/${year}`, {
-    headers: {
-      'content-type': 'application/json',
-      Authorization: `Bearer ${token}`
-    }
-  });
-  res
-    .json()
-    .then((res) => res)
-    .catch((err) => err);
-};
+export const year = moment(new Date()).format('YYYY');
--- src/shared/index.js
+++ src/shared/index.js
@@ -2,4 +2,4 @@ export { authContext } from './context/auth-context';
 export { default as AuthProvider } from './context/auth-context';
 export { useAuth } from './hooks/auth-hook';
 export { useFetch } from './hooks/fetch-hook';
-export { getGames, fetchData } from './helpers';
+export { year } from './helpers';
