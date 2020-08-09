8ba8979678ed63970a98142501cf72b524ece4bf
Malin Hanak <malinehanak@gmail.com>
2020-08-09 11:33:19 +0200

    Auth page

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -1,5 +1,7 @@
 import React, { useState, useContext } from 'react';
+import { Input } from '@material-ui/core';
 import { authContext } from '../shared';
+import '../styles/form-auth.scss';
 
 const Auth = (props) => {
   const auth = useContext(authContext);
@@ -7,11 +9,29 @@ const Auth = (props) => {
   const [password, setPassword] = useState('');
 
   return (
-    <>
-      <input value={team} onChange={(e) => setTeam(e.target.value)} />
-      <input value={password} onChange={(e) => setPassword(e.target.value)} />
-      <button onClick={() => auth.login({ name: team, password: password })}>Logga in</button>
-    </>
+    <form onSubmit={() => auth.login({ name: team, password: password })}>
+      <section className="wrapper">
+        <input
+          placeholder="Team namn"
+          type="text"
+          value={team}
+          onChange={(e) => setTeam(e.target.value)}
+        />
+        <div className="border"></div>
+      </section>
+      <section className="wrapper">
+        <input
+          placeholder="Password"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <div className="border"></div>
+      </section>
+      <button type="submit" disabled={!team || !password ? true : false}>
+        Logga in
+      </button>
+    </form>
   );
 };
 
