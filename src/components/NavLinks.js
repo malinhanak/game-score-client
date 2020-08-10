30d472835644b255e2e1b5b798e49bb7c6d6e239
Malin Hanak <malinehanak@gmail.com>
2020-08-10 14:48:39 +0200

    NavLinks style

--- src/components/NavLinks.js
+++ src/components/NavLinks.js
@@ -36,6 +36,14 @@ const Styles = styled.section`
     color: ${(props) => props.theme.colors.mineShaft};
     text-decoration: none;
     padding: 0.5rem;
+    font-size: 1.2rem;
+    font-weight: 500;
+    text-transform: uppercase;
+    margin: 0.9rem 0;
+  }
+
+  a:first-of-type {
+    margin: 0.9rem 0 0 0;
   }
 
   a:hover,
@@ -52,6 +60,11 @@ const Styles = styled.section`
     a {
       color: ${(props) => props.theme.colors.jetStream};
       text-decoration: none;
+      margin: 0;
+    }
+
+    a:first-of-type {
+      margin: 0;
     }
 
     a:hover,
