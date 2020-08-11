731d4708ee1f6d01f37515211b7937d33d309994
Malin Hanak <malinehanak@gmail.com>
2020-08-11 14:16:44 +0200

    cleaned up Layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { NavLinks } from './';
 import SideDrawer from './SideDrawer';
@@ -6,33 +6,27 @@ import Backdrop from './Backdrop';
 import Header from './Header';
 import Footer from './Footer';
 import { DrawerLinks } from '../styles';
-import { useFetch, year } from '../shared';
 
-const Layout = (props) => {
+const Layout = ({ children, links }) => {
   const [isOpen, setIsOpen] = useState(false);
-  const [links, error, isLoading] = useFetch(`/api/game/${year}`, null);
 
   const toggleDrawer = () => setIsOpen(!isOpen);
   const closeDrawer = () => setIsOpen(false);
 
-  useEffect(() => {}, [links, isLoading]);
-
   return (
-    links && (
-      <>
-        <Header toggleDrawer={toggleDrawer} links={links} />
-        {isOpen && (
-          <SideDrawer close={closeDrawer} isOpen={isOpen}>
-            <DrawerLinks>
-              <NavLinks close={closeDrawer} links={links} />
-            </DrawerLinks>
-          </SideDrawer>
-        )}
-        {isOpen && <Backdrop close={closeDrawer} />}
-        <Content>{props.children}</Content>
-        <Footer />
-      </>
-    )
+    <>
+      <Header toggleDrawer={toggleDrawer} links={links} />
+      {isOpen && (
+        <SideDrawer close={closeDrawer} isOpen={isOpen}>
+          <DrawerLinks>
+            <NavLinks close={closeDrawer} links={links} />
+          </DrawerLinks>
+        </SideDrawer>
+      )}
+      {isOpen && <Backdrop close={closeDrawer} />}
+      <Content>{children}</Content>
+      <Footer />
+    </>
   );
 };
 
