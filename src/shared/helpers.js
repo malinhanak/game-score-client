55faa958c93161aaf6fcd0a8f1c77934a6e723fe
Malin Hanak <malinehanak@gmail.com>
2020-08-11 10:54:02 +0200

    createSlug and getHeaders

--- src/shared/helpers.js
+++ src/shared/helpers.js
@@ -1,4 +1,24 @@
-import { useState } from 'react';
 import moment from 'moment';
 
 export const year = moment(new Date()).format('YYYY');
+
+export const createSlug = (name) => {
+  return name
+    ? name
+        .trim()
+        .replace(/[äåáà]/gim, 'a')
+        .replace(/[ööôóò]/gim, 'o')
+        .replace(/[èé]/gim, 'e')
+        .replace(/\s/g, '-')
+        .toLowerCase()
+    : null;
+};
+
+export const getHeaders = (cookies) => {
+  const headers = { 'content-type': 'application/json' };
+  if ((cookies && cookies.sid) || (cookies && cookies.token)) {
+    headers.Authorization = `Bearer ${cookies.sid.token}`;
+  }
+
+  return headers;
+};
