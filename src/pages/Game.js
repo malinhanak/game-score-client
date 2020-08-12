3827f60d151874334f8c8d42397e4df2b024d413
Malin Hanak <malinehanak@gmail.com>
2020-08-12 11:26:59 +0200

    Added the AddPoints component to Game Page

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -2,11 +2,13 @@ import React, { useEffect, useContext } from 'react';
 import renderHTML from 'react-render-html';
 import { useParams } from 'react-router-dom';
 
-import { useFetch, Store, GET_GAME_RULE } from '../shared';
+import { useFetch, Store, GET_GAME_RULE, authContext } from '../shared';
+import { AddPoints } from '../components';
 
 const Game = () => {
   const { game: name } = useParams();
   const { dispatch } = useContext(Store);
+  const auth = useContext(authContext);
   const [data, error, isLoading] = useFetch(GET_GAME_RULE(name), null);
 
   useEffect(() => {
@@ -24,6 +26,7 @@ const Game = () => {
       <h3>{name}</h3>
       {data && renderHTML(data.rules)}
       {error && error}
+      {auth.isOnline && <AddPoints game={name} />}
     </>
   );
 };
